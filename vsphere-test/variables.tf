ef47b683e312158627e295c8ebaa41f2b860e0aa
KALMOUN Abderrahmane <akalmoun@munisys.ma>
2026-01-06 21:42:10 +0100

    added vars

--- vsphere-test/variables.tf
+++ vsphere-test/variables.tf
@@ -1,26 +1,26 @@
 variable "vsphere_server" {
-  description = "vSphere server hostname or IP"
+  description = "vSphere server hostname or IP address (e.g., vcenter.example.com)"
   type        = string
 }
 
 variable "vsphere_user" {
-  description = "vSphere username"
+  description = "vSphere username for authentication (e.g., administrator@vsphere.local)"
   type        = string
 }
 
 variable "vsphere_password" {
-  description = "vSphere password"
+  description = "vSphere password for authentication"
   type        = string
   sensitive   = true
 }
 
 variable "datacenter_name" {
-  description = "Name of the vSphere datacenter"
+  description = "Name of the vSphere datacenter to query"
   type        = string
 }
 
 variable "allow_unverified_ssl" {
-  description = "Allow self-signed SSL certificates"
+  description = "Allow self-signed SSL certificates (set to true for test environments)"
   type        = bool
   default     = true
 }
